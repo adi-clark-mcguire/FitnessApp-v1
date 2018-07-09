3467579f3eae30bd21564797072021ed73b0382f
adi-clark-mcguire <adi.mcguire00@gmail.com>
2018-07-09 22:01:59 +0100

    Add files via upload
    Storyboard constraints in place

--- v2/fitnessapp-v2/fitnessapp-v2/LoginVC.swift
+++ v2/fitnessapp-v2/fitnessapp-v2/LoginVC.swift
@@ -10,14 +10,10 @@ import UIKit
 
 class LoginVC: UIViewController {
 
-
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
         
     }
 
@@ -25,8 +21,7 @@ class LoginVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
     
 }
 
