0bd4eba7aa63e053baab0386912614d4dd867d91
adi-clark-mcguire <adi.mcguire00@gmail.com>
2018-07-09 20:33:36 +0100

    Add files via upload

---
+++ v2/fitnessapp-v2/fitnessapp-v2/LoginVC.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  fitnessapp-v2
+//
+//  Created by Adi Clark-McGuire on 09/07/2018.
+//  Copyright © 2018 Adi Clark-McGuire. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+}
+
