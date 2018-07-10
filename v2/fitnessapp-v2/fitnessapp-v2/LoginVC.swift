34a9804fe1d1647e038f5b34f9f3e90115e4553e
adi-clark-mcguire <adi.mcguire00@gmail.com>
2018-07-10 01:01:29 +0100

    Add files via upload
    Swiping and layout is all working

--- v2/fitnessapp-v2/fitnessapp-v2/LoginVC.swift
+++ v2/fitnessapp-v2/fitnessapp-v2/LoginVC.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+       
     }
 
     override func didReceiveMemoryWarning() {
